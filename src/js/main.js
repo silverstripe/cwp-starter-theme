d8ebfd3981bd4ad899485d2e6e5dad3f133efb76
Paul Jayme <pjayme@silverstripe.com>
2017-01-12 16:28:23 +1300

    Added BS3 styling to $SearchForm + Imported BS3 JS for Collapse/Tab

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
 import share from './components/share';
+import collapse from 'bootstrap-sass/assets/javascripts/bootstrap/collapse'
+import tab from 'bootstrap-sass/assets/javascripts/bootstrap/tab'
 
 share();
