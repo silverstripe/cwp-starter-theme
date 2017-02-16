e5bda03346f797f51b186919bad2bb0eae040801
Robbie Averill <robbie@silverstripe.com>
2017-02-16 14:33:31 +1300

    CWPT-315: Export jQuery to allow the advanced theme to import it

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,15 @@
+// Define globally exposed module objects
+require('expose-loader?jquery!jquery');
+
+// Define dependency imports
 /* eslint-disable */
 import collapse from 'bootstrap-sass/assets/javascripts/bootstrap/collapse';
 import tab from 'bootstrap-sass/assets/javascripts/bootstrap/tab';
 import dropdown from 'bootstrap-sass/assets/javascripts/bootstrap/dropdown';
 import highlight from 'jquery-highlight/jquery.highlight';
 /* eslint-enable */
+
+// Define local components
 import navigation from './components/navigation';
 import content from './components/content';
 import sitemap from './components/sitemap';
