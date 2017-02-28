87029fc378ff722e38f6e80ed2a99a32bb9cd82f
Paul Jayme <pjayme@silverstripe.com>
2017-03-01 11:43:28 +1300

    Adding a new icon set as a new button in the CMS which allows user to apply icons on both the front-end and back-end

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,8 @@ mix.setPublicPath("dist");
 mix.js("src/js/main.js", "dist/js/main.js");
 mix
   .sass("src/scss/main.scss", "dist/css/main.css")
-  .sass("src/scss/_editor.scss", "dist/css/editor.css");
+  .sass("src/scss/_editor.scss", "dist/css/editor.css")
+  .sass("src/scss/_font-awesome.scss", "dist/css/font-awesome.css");
 
 mix.copy("node_modules/bootstrap-sass/assets/fonts/bootstrap", "dist/fonts");
 mix.copy("node_modules/font-awesome/fonts", "dist/fonts");
