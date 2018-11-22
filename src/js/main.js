4d292214b2fdd282ea03e4fa0b45d2ecb7a74af7
MasseyIsaako <lasiisaako@gmail.com>
2018-11-22 17:54:44 +1300

    feature/sd-7: fixed js issue. Have removed laravel-mix.

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,14 @@
 // Define globally exposed module objects
 /* eslint-disable */
 // Define dependency imports
-import util from 'bootstrap/js/dist/util';
-import collapse from 'bootstrap/js/dist/collapse';
-import dropdown from 'bootstrap/js/dist/dropdown';
-import carousel from 'bootstrap/js/dist/carousel';
-import tooltip from 'bootstrap/js/dist/tooltip';
-import popover from 'bootstrap/js/dist/popover';
-import modal from 'bootstrap/js/dist/modal';
-import tab from 'bootstrap/js/dist/tab';
+import util from 'bootstrap/js/src/util';
+import collapse from 'bootstrap/js/src/collapse';
+import dropdown from 'bootstrap/js/src/dropdown';
+import carousel from 'bootstrap/js/src/carousel';
+import tooltip from 'bootstrap/js/src/tooltip';
+import popover from 'bootstrap/js/src/popover';
+import modal from 'bootstrap/js/src/modal';
+import tab from 'bootstrap/js/src/tab';
 import highlight from 'jquery-highlight/jquery.highlight';
 
 // Define local components
